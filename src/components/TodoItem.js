be0e4cdf632771c64b83c16dd54618fbd3224ae9
Wilson <wjarizatruquetkd@gmail.com>
2019-06-14 23:32:26 -0500

    Added functionality
    Added functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 export class TodoItem extends Component {
+    getStyle= ()=>{
+      return{
+          background:'#f4f4f4',
+          padding:'10px',
+          borderBottom: '2px #ccc dotted',
+          textDecoration: this.props.todo.completed ? 'line-through':'none'
+      }
+    }
+  
+
     render() {
+        const{id,title}=this.props.todo;
         return (
-            <div>
-              <h3>{this.props.todo.title}</h3>  
+            <div style={this.getStyle()}>
+              <p>
+              <input type="checkbox" onChange={this.props.markComplete.bind(this,id)}/>{' '}
+              {title}
+              
+              </p>  
             </div>
         )
     }
 }
+
 
 
 TodoItem.propTypes={
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 class Todos extends Component {
+ 
   render() {
     return this.props.todos.map((todo)=>(
-       <TodoItem key={todo.id}todo={todo}/> 
+       <TodoItem key={todo.id}todo={todo} markComplete={this.props.markComplete}/> 
     ));
   }
 }
